ff6da96bb815f4470cf88064d81b77bea31f807d
Tony.Liang <pillar0514@163.com>
2014-10-20 10:23:26 +0800

    添加用于重力感应研究测试的DEMO

---
+++ public/test/demo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="zh-CN">
+<head>
+<?php include '../../../inc/header.php'; ?>
+<title>Orientation</title>
+</head>
+<body>
+<h1>Orientation</h1>
+<?php include '../../../inc/nav.php'; ?>
+<article>
+	<h5>Orientation API 测试</h5>
+	<ul>
+		<li><a href="test.html" target="_blank">基本测试</a></li>
+		<li><a href="coordinate-system.html" target="_blank">坐标系</a></li>
+	</ul>
+</article>
+<?php include '../../../inc/footer.php'; ?>
+</body>
+</html>
