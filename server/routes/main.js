5b9da7b7130cbcfc60b7c0af3fd835a5b6ed04ba
Tony.Liang <pillar0514@163.com>
2014-10-23 17:43:12 +0800

    添加可能会用到的项目文件

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,9 +2,18 @@ function index(req,res,next){
   res.render('index');
 }
 
+function sight(req,res,next){
+  res.render('sight');
+}
 
+function scene(req,res,next){
+  res.render('scene');
+}
 
 module.exports = function(app,io){
   app.get('/',index);
   app.get('/index',index);
+  app.get('/sight',sight);
+  app.get('/scene',scene);
 };
+
