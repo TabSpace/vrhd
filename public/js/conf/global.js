5608c10fc5ee64e11a9395780172de2ca5788471
Tony.Liang <pillar0514@163.com>
2014-10-27 09:59:14 +0800

    梳理各个页面导入的文件

--- public/js/conf/global.js
+++ public/js/conf/global.js
@@ -4,9 +4,7 @@
 	Tony.Liang <pillar0514@gmail.com>
  * @description 存放各个页面需要引入的公共模块
  */
-define('conf/page/index',function(require,exports,module){
-
-	require('conf/global');
+define('conf/global',function(require,exports,module){
 
 
 });
--- public/js/conf/page/vision.js
+++ public/js/conf/page/vision.js
@@ -4,7 +4,7 @@
 	Tony.Liang <pillar0514@gmail.com>
  * @description 用于PPT展示第一人称视角的同步影像，展示在PC上
  */
-define('conf/page/scene',function(require,exports,module){
+define('conf/page/vision',function(require,exports,module){
 
 	require('conf/global');
 
